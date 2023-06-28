f17469ceec116375eebeb9db9877b33a6a355c38
stuart <pkplonker@gmail.com>
2023-06-28 22:54:56 +0100

    First pass

---
+++ Assets/Editor/FenceSpawnerEditor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FenceSpawner))]
+public class FenceSpawnerEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		FenceSpawner fenceSpawner = (FenceSpawner)target;
+		if (GUILayout.Button("Spawn Fence"))
+		{
+			fenceSpawner.SpawnFence();
+		}
+	}
+}
---
+++ Assets/FenceSpawner.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceSpawner : MonoBehaviour
+{
+	[SerializeField] private GameObject postPrefab;
+	[SerializeField] private GameObject barPrefab;
+	[SerializeField] private float barLength;
+	[SerializeField] private float distance;
+	[SerializeField] private List<Vector3> points;
+
+	[SerializeField] private List<float> heights;
+
+	private GameObject fenceParent;
+
+	public void SpawnFence()
+	{
+		if (points.Count == 0 || heights.Count == 0) throw new ArgumentNullException();
+
+		if (fenceParent != null)
+			DestroyImmediate(fenceParent);
+
+		fenceParent = new GameObject("FenceParent");
+		fenceParent.transform.parent = this.transform;
+
+		for (var i = 0; i < points.Count; i++)
+		{
+			var startPoint = points[i];
+			var endPoint = points[(i + 1) % points.Count];
+			var direction = (endPoint - startPoint).normalized;
+			var distance = Vector3.Distance(startPoint, endPoint);
+			var quantity = Mathf.CeilToInt(distance / barLength);
+			var remainder = distance % barLength;
+
+			for (var j = 0; j < quantity; j++)
+			{
+				var position = startPoint + direction * (j * barLength);
+				Instantiate(postPrefab, position, Quaternion.identity, fenceParent.transform);
+
+				foreach (var height in heights)
+				{
+					var barPosition = startPoint + direction * ((j + 0.5f) * barLength + (barLength * 0.5f));
+					barPosition.y += height;
+					var bar = Instantiate(barPrefab, barPosition, Quaternion.LookRotation(direction),
+						fenceParent.transform);
+					if (j != quantity - 1) continue;
+
+					var barScale = bar.transform.localScale;
+					barScale.z *= remainder / barLength;
+					bar.transform.localScale = barScale;
+
+					float adjustment = (barLength * barScale.z);
+					bar.transform.position = startPoint + direction * ((j + 0.5f) * barLength + (barLength * 0.5f)) -
+					                         (direction * (barLength - adjustment)) +
+					                         new Vector3(0, height, 0);
+				}
+			}
+		}
+	}
+}
